efef1cd40bb6220cafbcb8898d4f2c6da7c8d2b8
obarrott <71147609+obarrott@users.noreply.github.com>
2020-09-23 13:52:19 -0600

    added a post model and a post controller to fetch data.

--- Reddit/Reddit/Controllers/Model Controllers/PostController.swift
+++ Reddit/Reddit/Controllers/Model Controllers/PostController.swift
@@ -8,6 +8,7 @@
 
 // https://www.reddit.com/r/funny/.json
 import Foundation
+import UIKit.UIImage
 
 struct StringConstants {
     fileprivate static let baseURL = "https://www.reddit.com" //fileprivate allows this struct to only be accsessed within this file. "private" only allows it within the class
@@ -30,6 +31,46 @@ class PostController {
         let finalURL = funnyComponentURL.appendingPathExtension(StringConstants.jsonExtension)
         print(finalURL) //Always do this to make sure that your URL is working.
         
-        }
+        URLSession.shared.dataTask(with: finalURL) { (data, _, error) in
+            if let error = error {
+                return completion(.failure(.thrownError(error)))
+            }
+            
+            guard let data = data else { return completion(.failure(.noData)) }
+            
+            do {
+                let topLevelDictionary = try JSONDecoder().decode(TopLevelDictionary.self, from: data)
+                let secondLevelDictionary = topLevelDictionary.data
+                let thirdLevelObjects = secondLevelDictionary.children
+                
+                var postsPlaceholderArray: [Post] = []
+                for object in thirdLevelObjects{
+                    let post = object.data
+                    postsPlaceholderArray.append(post)
+                }
+                completion(.success(postsPlaceholderArray))
+            } catch {
+                return completion(.failure(.thrownError(error)))
+            }
+        }.resume()
     }
+    
+    static func fetchThumbnailFor(post: Post, completion: @escaping(Result<UIImage, PostError>) -> Void) {
+        guard let url = post.thumbnail else {return completion(.failure(.invalidURL))}
+        print(url)
+        
+        URLSession.shared.dataTask(with: url) { (data, _, error) in
+            if let error = error {
+                return completion(.failure(.thrownImageError(error)))
+            }
+            
+            guard let data = data else {return completion(.failure(.noData))}
+            //The UIImage method decodes underneath the hood, but it doesn't throw. That's why we don't have to put it in a do-catch block.
+            guard let thumbnailImage = UIImage(data: data) else {return completion(.failure(.unableToDecode))}
+                
+                completion(.success(thumbnailImage))
+        }.resume()
+    }
+}
+
     
